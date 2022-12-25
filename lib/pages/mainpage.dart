7054000f517baff31fda94205d9da50ff1353bfe
Jab1k <jobir4166@gmail.com>
2022-12-25 17:13:21 +0500

    Tugadi

--- lib/pages/mainpage.dart
+++ lib/pages/mainpage.dart
@@ -1,23 +1,118 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:http/http.dart' as http;
+import 'package:flutter_application_5/store/flags.dart';
 
-class Nbu extends StatefulWidget {
-  const Nbu({super.key});
+import '../repasitory/infoget.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<Nbu> createState() => _NbuState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _NbuState extends State<Nbu> {
+class _HomePageState extends State<HomePage> {
+  List data = [];
+  List flag = [];
+  GetInformationRepository api = GetInformationRepository();
+  bool isLoading = false;
+  Future<void> getInformation() async {
+    isLoading = true;
+    setState(() {});
+    data = await api.getInformation();
+    isLoading = false;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    getInformation();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xffD3F3FF),
-      body: Column(
-        children: [],
-      ),
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    width: double.infinity,
+                    margin: const EdgeInsets.symmetric(horizontal: 24),
+                    decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(32),
+                            topRight: Radius.circular(32)),
+                        color: Colors.white),
+                    child: ListView.builder(
+                      itemCount: data.length - 1,
+                      itemBuilder: (context, index) {
+                        return Column(
+                          children: [
+                            Container(
+                              height: 50,
+                              width: double.infinity,
+                              margin:
+                                  const EdgeInsets.symmetric(horizontal: 24),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(24),
+                                  color: Colors.white),
+                              child: Row(
+                                children: [
+                                  Container(
+                                    height: 40,
+                                    width: 60,
+                                    decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                        fit: BoxFit.cover,
+                                        image: NetworkImage(
+                                          returnFlag(index),
+                                        ),
+                                      ),
+                                      borderRadius: const BorderRadius.only(
+                                        topRight: Radius.circular(12),
+                                        bottomLeft: Radius.circular(12),
+                                      ),
+                                      color: Colors.blue,
+                                    ),
+                                  ),
+                                  const SizedBox(width: 10),
+                                  Text(
+                                    data[index].code ?? '',
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 24,
+                                    ),
+                                  ),
+                                  const SizedBox(width: 10),
+                                  Text(
+                                    data[index].title ?? '',
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.w300,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 24),
+                              child: SizedBox(
+                                width: double.infinity,
+                                child: Divider(),
+                              ),
+                            ),
+                            const SizedBox(height: 10)
+                          ],
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
     );
   }
 }
--- lib/repasitory/infoget.dart
+++ lib/repasitory/infoget.dart
@@ -1,16 +1,23 @@
 import 'dart:convert';
+import 'dart:math';
 import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
 import '../model/model.dart';
 
 class GetInformationRepository {
-  getInformation({required String name}) async {
+  getInformation() async {
     try {
-      final url = Uri.parse('https://api.genderize.io/?name=$name');
-      final res = await http.get(url);
-      dynamic data = jsonDecode(res.body);
-      Nbu names = Nbu.fromJson(data);
-      return names;
+      Response response =
+          await http.get(Uri.parse('https://nbu.uz/uz/exchange-rates/json/'));
+      if (response.statusCode == 200) {
+        var jsonData = jsonDecode(response.body);
+        List<Nbu> categories =
+            (jsonData as List?)?.map((e) => Nbu.fromJson(e)).toList() ?? [];
+        return categories;
+      } else {
+        throw Exception();
+      }
     } catch (e) {
       print(e);
     }
